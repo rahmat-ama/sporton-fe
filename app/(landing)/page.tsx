d129716bf380c3192c4a0619332924d9a2e60d25
Rahmat Amalul <rahmatamalul@gmail.com>
2026-01-05 06:16:55 +0700

    konfig dasar

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="bg-primary">
+      <h1 className="font-extrabold">Halo apakah ini poppins</h1>
+    </main>
+  );
+}
