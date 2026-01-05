2b3ec6e1497fe82c0b8ba337b3e8d5c8b25f9239
Rahmat Amalul <rahmatamalul@gmail.com>
2026-01-05 19:13:19 +0700

    add categories section

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Button from "./components/ui/button";
 import HeroSection from "./components/home/hero";
+import CategoriesSection from "./components/home/categories";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <CategoriesSection />
     </main>
   );
 }
