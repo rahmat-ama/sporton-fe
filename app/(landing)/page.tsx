3350d9f2a7e982e022488432b9ec2c52b5d192da
Rahmat Amalul <rahmatamalul@gmail.com>
2026-01-05 22:16:32 +0700

    add product section

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Button from "./components/ui/button";
 import HeroSection from "./components/home/hero";
 import CategoriesSection from "./components/home/categories";
+import ProductsSection from "./components/home/products";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <CategoriesSection />
+      <ProductsSection />
     </main>
   );
 }
